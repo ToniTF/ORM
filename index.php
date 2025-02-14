15d5dc90e643fd5760c646d8a5f1350466979d52
Tonitf <subcero00@hotmail.com>
2025-02-14 17:50:08 +0100

    salvar nuevo actor

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ require 'vendor/autoload.php';
 require 'database.php';
 
 use App\Models\Actor;
+$actor = new Actor();
+$actor->first_name = 'ANTONIO';
+$actor->last_name = 'TROITIÑO';
+$actor->save();
 
 // Obtener todos los registros
 $actors = Actor::all();
